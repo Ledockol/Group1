8443debd6dd6b928ae4d42dd2897dba197df1413
Andrey <andrey-ssk98@yandex.ru>
2025-12-05 19:16:58 +0300

    Even Sort correction

--- src/strategy/Even/EvenSortStrategy.java
+++ src/strategy/Even/EvenSortStrategy.java
@@ -2,10 +2,7 @@ package strategy.Even;
 
 import strategy.SortStrategy;
 
-import java.util.List;
 
 public interface EvenSortStrategy<T> extends SortStrategy<T> {
-    void sort(List<T> items);
-
     boolean isEven(T item);
 }
