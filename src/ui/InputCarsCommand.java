8854c7f5894a856e3974b0ae3b33b854dd8f2456
german-man <artemjev.german@yandex.ru>
2025-12-16 23:48:14 +0300

    ввод нескольких автомобилей сразу

--- src/ui/InputCarsCommand.java
+++ src/ui/InputCarsCommand.java
@@ -2,6 +2,7 @@ package ui;
 
 import io.CarInput;
 import models.Car;
+import java.util.Scanner;
 
 public class InputCarsCommand extends BaseCommand {
     public InputCarsCommand(CarStorage storage) {
@@ -10,20 +11,33 @@ public class InputCarsCommand extends BaseCommand {
 
     @Override
     public void execute() {
+        System.out.println("--- Ручной ввод автомобилей ---");
 
-        System.out.println("--- Ввод автомобиля ---");
-
-        Car targetCar = new CarInput().inputCar();
-
-        if (targetCar == null) {
-            System.out.println("Ввод отменен.");
-            return;
-        }
+        var scanner = new Scanner(System.in);
 
         var cars = getStorage().getCars();
 
-        cars.add(targetCar);
+        while (true) {
+            System.out.println("Введите данные нового автомобиля:");
+
+            var targetCar = new CarInput().inputCar();
+
+            if (targetCar != null) {
+                cars.add(targetCar);
+                System.out.println("Автомобиль успешно добавлен в список.");
+            } else {
+                System.out.println("Ввод этого автомобиля был отменен.");
+            }
+
+            System.out.print("Прекратить ввод? (да/нет): ");
+            String answer = scanner.nextLine().trim().toLowerCase();
+
+            if (answer.equals("да") || answer.equals("y") || answer.equals("yes")) {
+                break;
+            }
+        }
 
         getStorage().setCars(cars);
+        System.out.println("--- Ввод завершен. Всего машин в базе: " + cars.size() + " ---");
     }
 }
